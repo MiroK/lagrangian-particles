edbca39ca3270b7a004bfb3e80ac666077f9e8d0
mirok <miroslav.kuchta@gmail.com>
2014-11-21 09:56:46 +0100

    init

---
+++ LagrangianParticles.py
@@ -0,0 +1,400 @@
+#__authors__ = ('Mikael Mortensen <mikaem@math.uio.no>',
+#               'Miroslav Kuchta <mirok@math.uio.no>')
+#__date__ = '2014-19-11'
+#__copyright__ = 'Copyright (C) 2011' + __authors__
+#__license__  = 'GNU Lesser GPL version 3 or any later version'
+'''
+This module contains functionality for Lagrangian tracking of particles with
+DOLFIN
+'''
+
+import dolfin as df
+import numpy as np
+import copy
+import ufc
+from mpi4py import MPI as pyMPI
+from collections import defaultdict
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+
+comm = pyMPI.COMM_WORLD
+
+
+class Particle:
+    'Lagrangian particle with position and some other passive properties.'
+    def __init__(self, x):
+        self.position = x
+        self.properties = {}
+
+    def send(self, dest):
+        'Send particle to dest.'
+        comm.Send(self.position, dest=dest)
+        comm.send(self.properties, dest=dest)
+
+    def recv(self, source):
+        'Receive info of a new particle sent from source.'
+        comm.Recv(self.position, source=source)
+        self.properties = comm.recv(source=source)
+
+
+class CellWithParticles(df.Cell):
+    'Dolfin cell with list of particles that it contains.'
+    def __init__(self, mesh, cell_id, particle):
+        # Initialize parent -- create Cell with id on mesh
+        df.Cell.__init__(self, mesh, cell_id)
+        # Make an empty list of particles that I carry
+        self.particles = []
+        self += particle
+
+    def __add__(self, particle):
+        'Add single particle to cell.'
+        assert isinstance(particle, (Particle, np.ndarray))
+        if isinstance(particle, Particle):
+            self.particles.append(particle)
+            return self
+        else:
+            return self.__add__(Particle(particle))
+
+    def __len__(self):
+        'Number of particles in cell.'
+        return len(self.particles)
+
+
+class CellParticleMap(dict):
+    'Dictionary of cells with particles.'
+    def __add__(self, ins):
+        '''
+        Add ins to map:
+            ins is either (mesh, cell_id, particle) or
+                          (mesh, cell_id, particle, particle_properties)
+        '''
+        assert isinstance(ins, tuple) and len(ins) in (3, 4)
+        # If the cell_id is in map add the particle
+        if ins[1] in self:
+            self[ins[1]] += ins[2]
+        # Other wise create new cell
+        else:
+            self[ins[1]] = CellWithParticles(ins[0], ins[1], ins[2])
+        # With particle_properties, update properties of the last added particle
+        if len(ins) == 4:
+            self[ins[1]].particles[-1].properties.update(ins[3])
+
+        return self
+
+    def pop(self, cell_id, i):
+        'Remove i-th particle from the list of particles in cell with cell_id.'
+        # Note that we don't check for cell_id being a key or cell containg
+        # at least i particles.
+        particle = self[cell_id].particles.pop(i)
+
+        # If the cell is empty remove it from map
+        if len(self[cell_id]) == 0:
+            del self[cell_id]
+
+        return particle
+
+    def total_number_of_particles(self):
+        'Total number of particles in all cells of the map.'
+        return sum(map(len, self.itervalues()))
+
+
+class LagrangianParticles:
+    'Particles moved by the velocity field in V.'
+    def __init__(self, V):
+        self.__debug = True
+
+        self.V = V
+        self.mesh = V.mesh()
+        self.mesh.init(2, 2)  # Cell-cell connectivity for neighbors of cell
+        self.tree = self.mesh.bounding_box_tree()  # Tree for isection comput.
+
+        # Allocate some variables used to look up the velocity
+        # Velocity is computed as U_i*basis_i where i is the dimension of
+        # element function space, U are coefficients and basis_i are element
+        # function space basis functions. For interpolation in cell it is
+        # advantageous to compute the resctriction once for cell and only
+        # update basis_i(x) depending on x, i.e. particle where we make
+        # interpolation. This updaea mounts to computing the basis matrix
+        self.ufc_cell = ufc.cell()
+        self.dim = self.mesh.topology().dim()
+
+        self.element = V.dolfin_element()
+        self.num_tensor_entries = 1
+        for i in range(self.element.value_rank()):
+            self.num_tensor_entries *= self.element.value_dimension(i)
+        # For VectorFunctionSpace CG1 this is 3
+        self.coefficients = np.zeros(self.element.space_dimension())
+        # For VectorFunctionSpace CG1 this is 3x3
+        self.basis_matrix = np.zeros((self.element.space_dimension(),
+                                      self.num_tensor_entries))
+
+        # Allocate a dictionary to hold all particles
+        self.particle_map = CellParticleMap()
+
+        # Allocate some MPI stuff
+        self.num_processes = comm.Get_size()
+        self.myrank = comm.Get_rank()
+        self.all_processes = range(self.num_processes)
+        self.other_processes = range(self.num_processes)
+        self.other_processes.remove(self.myrank)
+        self.my_escaped_particles = np.zeros(1, dtype='I')
+        self.tot_escaped_particles = np.zeros(self.num_processes, dtype='I')
+        # Dummy particle for receiving/sending at [0, 0, ...]
+        self.particle0 = Particle(np.zeros(self.mesh.geometry().dim()))
+
+    def add_particles(self, list_of_particles, properties_d=None):
+        '''Add particles and search for their home on all processors.
+           Note that list_of_particles must be same on all processes. Further
+           every len(properties[property]) must equal len(list_of_particles).
+        '''
+        if properties_d is not None:
+            n = len(list_of_particles)
+            assert all(len(sub_list) == n
+                       for sub_list in properties_d.itervalues())
+            # Dictionary that will be used to feed properties of single
+            # particles
+            properties = properties_d.keys()
+            particle_properties = dict((key, 0) for key in properties)
+
+            has_properties = True
+        else:
+            has_properties = False
+
+        pmap = self.particle_map
+        my_found = np.zeros(len(list_of_particles), 'I')
+        all_found = np.zeros(len(list_of_particles), 'I')
+        for i, particle in enumerate(list_of_particles):
+            c = self.locate(particle)
+            if not c == -1:
+                my_found[i] = True
+                if not has_properties:
+                    pmap += self.mesh, c, particle
+                else:
+                    # Get values of properties for this particle
+                    for key in properties:
+                        particle_properties[key] = properties_d[key][i]
+                    pmap += self.mesh, c, particle, particle_properties
+        # All particles must be found on some process
+        comm.Reduce(my_found, all_found, root=0)
+        if self.myrank == 0:
+            missing = np.where(all_found == 0)[0]
+            n_missing = len(missing)
+            if self.__debug:
+                for i in missing:
+                    print 'Missing', list_of_particles[i].position
+
+            assert n_missing == 0,\
+                '%d particles are not located in mesh' % n_missing
+
+            n_duplicit = len(np.where(all_found > 1)[0])
+            print 'There are %d duplicit particles' % n_duplicit
+
+    def step(self, u, dt):
+        'Move particles by forward Euler x += u*dt'
+        for cwp in self.particle_map.itervalues():
+            # Restrict once per cell
+            u.restrict(self.coefficients,
+                       self.element,
+                       cwp,
+                       cwp.get_vertex_coordinates(),
+                       self.ufc_cell)
+            for particle in cwp.particles:
+                x = particle.position
+                # Compute velocity at position x
+                self.element.evaluate_basis_all(self.basis_matrix,
+                                                x,
+                                                cwp.get_vertex_coordinates(),
+                                                cwp.orientation())
+                x[:] = x[:] + dt*np.dot(self.coefficients, self.basis_matrix)[:]
+        # Recompute the map
+        self.relocate()
+
+    def relocate(self):
+        # Relocate particles on cells and processors
+        p_map = self.particle_map
+        # Map such that map[old_cell] = [(new_cell, particle_id), ...]
+        # Ie new destination of particles formerly in old_cell
+        new_cell_map = defaultdict(list)
+        for cwp in p_map.itervalues():
+            for i, particle in enumerate(cwp.particles):
+                point = Point(*particle.position)
+                # Search only if particle moved outside original cell
+                if not cwp.contains(point):
+                    found = False
+                    # Check neighbor cells
+                    for neighbor in df.cells(cwp):
+                        if neighbor.contains(point):
+                            new_cell_id = neighbor.index()
+                            found = True
+                            break
+                    # Do a completely new search if not found by now
+                    if not found:
+                        new_cell_id = self.locate(particle)
+                    # Record to map
+                    new_cell_map[cwp.index()].append((new_cell_id, i))
+
+        # Rebuild locally the particles that end up on the process. Some
+        # have cell_id == -1, i.e. they are on other process
+        list_of_escaped_particles = []
+        for old_cell_id, new_data in new_cell_map.iteritems():
+            # We iterate in reverse becasue normal order would remove some
+            # particle this shifts the whole list!
+            for (new_cell_id, i) in sorted(new_data,
+                                           key=lambda t: t[1],
+                                           reverse=True):
+                particle = p_map.pop(old_cell_id, i)
+                if new_cell_id == -1:
+                    list_of_escaped_particles.append(particle)
+                else:
+                    p_map += self.mesh, new_cell_id, particle
+
+        # Create a list of how many particles escapes from each processor
+        self.my_escaped_particles[0] = len(list_of_escaped_particles)
+        # Make all processes aware of the number of escapees
+        comm.Allgather(self.my_escaped_particles, self.tot_escaped_particles)
+
+        # Send particles to root
+        for particle in list_of_escaped_particles:
+            particle.send(0)
+
+        # Receive the particles escaping from other processors
+        if self.myrank == 0:
+            for proc in self.other_processes:
+                for i in range(self.tot_escaped_particles[proc]):
+                    self.particle0.recv(proc)
+                    list_of_escaped_particles.append(copy.deepcopy(self.particle0))
+
+        # Put all travelling particles on all processes, then perform new search
+        travelling_particles = comm.bcast(list_of_escaped_particles, root=0)
+        self.add_particles(travelling_particles)
+
+    def total_number_of_particles(self):
+        'Return number of particles in total and on process.'
+        num_p = self.cellparticles.total_number_of_particles()
+        tot_p = comm.allreduce(num_p)
+        return (tot_p, num_p)
+
+    def locate(self, particle):
+        'Find mesh cell that contains particle.'
+        assert isinstance(particle, (Particle, np.ndarray))
+        if isinstance(particle, Particle):
+            # Convert particle to point
+            point = df.Point(*particle.position)
+            return self.tree.compute_first_entity_collision(point)
+        else:
+            return self.locate(Particle(particle))
+
+    def scatter(self, fig, skip=1):
+        'Scatter plot of all particles on process 0'
+        ax = fig.gca()
+
+        p_map = self.particle_map
+        all_particles = np.zeros(self.num_processes, dtype='I')
+        my_particles = p_map.total_number_of_particles()
+        # Root learns about count of particles on all processes
+        comm.Gather(np.array([my_particles], 'I'), all_particles, root=0)
+
+        # Slaves should send to master
+        if self.myrank > 0:
+            for cwp in p_map.itervalues():
+                for p in cwp.particles:
+                    p.send(0)
+        else:
+            # Receive on master
+            received = defaultdict(list)
+            received[0] = [copy.copy(p.position)
+                           for cwp in p_map.itervalues()
+                           for p in cwp.particles]
+            for proc in self.other_processes:
+                # Receive all_particles[proc]
+                for j in range(all_particles[proc]):
+                    self.particle0.recv(proc)
+                    received[proc].append(copy.copy(self.particle0.position))
+
+            cmap = plt.get_cmap('jet')
+            cnorm = colors.Normalize(vmin=0, vmax=self.num_processes)
+            scalarMap = cmx.ScalarMappable(norm=cnorm, cmap=cmap)
+
+            for proc in received:
+                # Plot only if there is something to plot
+                particles = received[proc]
+                if len(particles) > 0:
+                    xy = np.array(particles)
+
+                    ax.scatter(xy[::skip, 0], xy[::skip, 1],
+                               label='%d' % proc,
+                               c=scalarMap.to_rgba(proc))
+            ax.legend(loc='best')
+            ax.axis([0, 1, 0, 1])
+
+# -----------------------------SEEDING----------------------------------------
+
+def circle(center, radius, N):
+    '''
+    Put N particles on circle with radius centered at center.
+    '''
+    pi = df.pi
+    theta = np.linspace(0, 2.*pi, N, endpoint=False)
+    r = np.linspace(0,radius,10)
+    xs, ys = center[0] + radius*np.cos(theta), center[1] + radius*np.sin(theta)
+    return [np.array([x, y]) for x, y, in zip(xs, ys)]
+
+def hat(x0,x1,y0,y1,N):
+	"""
+	Put N particles at random poisitions inside a Rectangle
+	"""
+	xs = []
+	ys = []
+	for i in range(N):
+		xrand = np.random.uniform(x0,x1)
+		yrand = np.random.uniform(y0,y1)
+		xs.append(xrand)
+		ys.append(yrand)
+
+	return [np.array([x, y]) for x, y, in zip(xs, ys)]
+
+
+
+# -------------------------------MAIN-----------------------------------------
+
+if __name__ == '__main__':
+    from dolfin import UnitSquareMesh, Point, VectorFunctionSpace, interpolate,\
+        Constant
+    import matplotlib.pyplot as plt
+
+    mesh = df.RectangleMesh(0,0,1,1,10,10)
+    particle_positions = circle([0.5, 0.75], 0.15, 200)
+    #particle_positions = hat(0.8,1.2,0,0.4,10000)
+
+    V = VectorFunctionSpace(mesh, 'CG', 1)
+    lp = LagrangianParticles(V)
+    lp.add_particles(particle_positions)
+    u = interpolate(df.Expression(("-2*sin(pi*x[1])*cos(pi*x[1])*pow(sin(pi*x[0]),2)",
+                                   "2*sin(pi*x[0])*cos(pi*x[0])*pow(sin(pi*x[1]),2)")),
+                    V)
+    # u_expression = df.Expression(("sin(2*t*pi)","0"), t=0.0)
+    # u = df.Function(V)
+    # u.interpolate(u_expression)
+    # df.plot(u)
+
+    fig = plt.figure()
+    fig.show()
+    lp.scatter(fig)
+    fig.suptitle('Initial')
+    plt.ion()
+
+    # -->
+    dt = 0.01
+    for step in range(500):
+	t = step*dt
+	#u_expression.t = t
+	#u = interpolate(u_expression,V)
+	#df.plot(u)
+        lp.step(u, dt=dt)
+        #fig = plt.figure()
+        lp.scatter(fig)
+        fig.suptitle('At step %d' % step)
+
+	plt.draw()
+	fig.clf()
+

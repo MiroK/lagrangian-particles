7c9b3e6c52c05df67a8b03bc132b176dc9f88e3e
mirok <miroslav.kuchta@gmail.com>
2014-11-21 21:27:10 +0100

    demo_statistics

--- LagrangianParticles.py
+++ LagrangianParticles.py
@@ -231,7 +231,7 @@ class LagrangianParticles:
         new_cell_map = defaultdict(list)
         for cwp in p_map.itervalues():
             for i, particle in enumerate(cwp.particles):
-                point = Point(*particle.position)
+                point = df.Point(*particle.position)
                 # Search only if particle moved outside original cell
                 if not cwp.contains(point):
                     found = False
@@ -325,7 +325,7 @@ class LagrangianParticles:
                     self.particle0.recv(proc)
                     received[proc].append(copy.copy(self.particle0.position))
 
-            cmap = plt.get_cmap('jet')
+            cmap = cmx.get_cmap('jet')
             cnorm = colors.Normalize(vmin=0, vmax=self.num_processes)
             scalarMap = cmx.ScalarMappable(norm=cnorm, cmap=cmap)
 
@@ -360,118 +360,4 @@ class LagrangianParticles:
         else:
             return None
 
-
-# -----------------------------------------------------------------------------
-
-if __name__ == '__main__':
-    from dolfin import Point, VectorFunctionSpace, interpolate
-    from particle_generators import RandomRectangle
-    import matplotlib.pyplot as plt
-
-    mesh = df.RectangleMesh(0, 0, 1, 1, 10, 10)
-    particle_positions = RandomRectangle(0.125, 0.25, 0.75, 0.8).generate([100,
-                                                                           100])
-
-    V = VectorFunctionSpace(mesh, 'CG', 1)
-    lp = LagrangianParticles(V)
-    lp.add_particles(particle_positions)
-    u = interpolate(df.Expression(("-2*sin(pi*x[1])*cos(pi*x[1])*pow(sin(pi*x[0]),2)",
-                                   "2*sin(pi*x[0])*cos(pi*x[0])*pow(sin(pi*x[1]),2)")),
-                    V)
-
-    # Initialize scatter plot
-    fig0 = plt.figure()
-    lp.scatter(fig0)
-    fig0.suptitle('Initial')
-    # Initialize bar plot
-    fig1 = plt.figure()
-    lp.bar(fig1)
-    fig1.suptitle('Initial')
-
-    plt.ion()
-
-    # Data for comunicating cpu time and particles count
-    my_cpu = np.zeros(1, 'float')
-    all_cpu = np.zeros(comm.Get_size(), 'float')
-
-    my_count = np.zeros(1, 'I')
-    all_count = np.zeros(comm.Get_size(), 'I')
-
-    # Steps to be used in computation
-    steps = np.arange(500)
-
-    if comm.Get_rank() == 0:
-        procs = range(comm.Get_size())
-        # Data for history of cpu time per step and particle count
-        cpu_histories = np.zeros((len(steps), len(procs)))
-        count_histories = np.zeros_like(cpu_histories)
-        # Similar to scatter we color history curve by proces it belongs to
-        cmap = plt.get_cmap('jet')
-        cnorm = colors.Normalize(vmin=0, vmax=len(procs))
-        scalarMap = cmx.ScalarMappable(norm=cnorm, cmap=cmap)
-        # Two subplots, one for histories of cpu time, the other for particle
-        # count
-        fig2, axarr = plt.subplots(2, sharex=True)
-        lines = [[axarr[i].plot(steps,
-                                steps,
-                                label='%d' % proc,
-                                c=scalarMap.to_rgba(proc))[0]
-                  for proc in procs]
-                 for i in range(2)]
-        # Guess this should be enough for time step
-        axarr[0].set_ylim([-0.1, 2])
-        axarr[0].set_ylabel('s/step')
-        # We share axes so remove labels of x-axies
-        plt.setp([a.get_xticklabels() for a in fig2.axes[:0]], visible=False)
-        # Particle loaf handles the x-axes
-        axarr[1].set_ylim([-10, len(particle_positions)+10])
-        axarr[1].set_ylabel('number of particles')
-        axarr[1].set_xlabel('step')
-        # One legend shared for both plots
-        axarr[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.25),
-                        ncol=len(procs))
-        # Put plots togeter
-        fig2.subplots_adjust(hspace=0.05)
-
-        fig0.show()
-        fig1.show()
-        fig2.show()
-
-    dt = 0.01
-    for step in steps:
-        # Communicate the cpu time
-        my_cpu[0] = sum(lp.step(u, dt=dt))
-        comm.Gather(my_cpu, all_cpu, root=0)
-        # Communicate particle load
-        my_count[0] = lp.total_number_of_particles()[1]
-        comm.Gather(my_count, all_count, root=0)
-
-        # Update history plot
-        if comm.Get_rank() == 0:
-
-            cpu_histories[step, :] = all_cpu
-            count_histories[step, :] = all_count
-
-            for proc in procs:
-                lines[0][proc].set_ydata(cpu_histories[:, proc])
-
-            for proc in procs:
-                lines[1][proc].set_ydata(count_histories[:, proc])
-
-            fig2.canvas.draw()
-
-        # Update scatter
-        lp.scatter(fig0)
-        fig0.suptitle('At step %d' % step)
-
-        # Update bar plot
-        n_particles = lp.bar(fig1)
-        if n_particles is not None:
-            fig1.suptitle('At step %d, total particles %d' % (step,
-                                                              n_particles))
-
-        fig0.canvas.draw()
-        fig0.clf()
-        fig1.canvas.draw()
-        fig1.clf()
 

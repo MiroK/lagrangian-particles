7b307fae61497f3a8cf93f9378c6f26f2629e3b3
Per Thomas Haga <perthaga@gmail.com>
2015-03-22 02:34:14 +0100

    update

--- demo_plain.py
+++ demo_plain.py
@@ -1,14 +1,16 @@
 from LagrangianParticles import LagrangianParticles
-from particle_generators import RandomCircle
+from particle_generators import RandomCircle, circle
 import matplotlib.pyplot as plt
-from dolfin import VectorFunctionSpace, interpolate, RectangleMesh, Expression
+from dolfin import VectorFunctionSpace, interpolate, RectangleMesh, Expression, Function
 from mpi4py import MPI as pyMPI
 import time
 
 comm = pyMPI.COMM_WORLD
 
 mesh = RectangleMesh(0, 0, 1, 1, 20, 20)
-particle_positions = RandomCircle([0.5, 0.75], 0.15).generate([50, 50])
+#particle_positions = RandomCircle([0.5, 0.75], 0.15).generate([50, 50])
+particle_positions = circle([0.5, 0.75], 0.15, 200)
+
 
 V = VectorFunctionSpace(mesh, 'CG', 1)
 u_plus = interpolate(Expression(("-2*sin(pi*x[1])*cos(pi*x[1])*pow(sin(pi*x[0]),2)",
@@ -31,7 +33,6 @@ if comm.Get_rank() == 0:
     fig0.show()
 
 
-
 #plt.ion()
 
 dt = 0.001
@@ -49,13 +50,15 @@ while t <= T:
         u = u_plus
     else:
         u = u_minus
-    lp.step(u, t, dt, V)
+    
+    lp.step(u,dt)
 
-    #if step % 100 == 0:
-        #lp.scatter(fig)
-        #fig.suptitle('At step %d' % step)
-        #fig.canvas.draw()
-        #fig.clf()
+    if step % 100 == 0:
+        lp.scatter(fig)
+        fig.suptitle('At step %d' % step)
+        fig.canvas.draw()
+        fig.clf()
+
 if comm.Get_rank() == 0:
     t1 = time.time()
     print "Total time: ", t1-t0
